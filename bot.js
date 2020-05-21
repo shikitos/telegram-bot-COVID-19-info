6d296cd206849cd689a2d7578edf44ec243ba0eb
Nikita Shitov <56273378+shikitos@users.noreply.github.com>
2020-05-21 16:58:14 +0500

    Add reportsByCountry and byDeaths
    Add country reports by sending a message to the bot and it responds with statistics - it is working good but I should change event on('text') to the some different, because I can't ask my bot for the over things, cuz he always thinks what I am asking him about reportsByCountry and send me "Error. Wrong country!"

--- bot.js
+++ bot.js
@@ -1,12 +1,61 @@
 /* eslint-disable prettier/prettier */
 require('dotenv').config();
 const Telegraf = require('telegraf');
+const markup = require('telegraf/markup');
+const api = require('covid19-api');
+const country = require('constants');
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start((ctx) => ctx.reply('Welcome'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.start((ctx) =>
+    ctx.reply(
+        'Welcome',
+        markup
+        .keyboard([
+            ['US', 'Russia'],
+            ['Hungary', 'UK'],
+        ])
+        .resize()
+        .extra()
+    )
+);
+//Help
+// bot.help((ctx) => ctx.reply('ol'));
+//a person writes a country, in response to the number of cases, deaths, and recoveries
+bot.hears(country, async(ctx) => {
+    let data = {};
+    try {
+        data = await api.getReportsByCountries(ctx.message.text);
+        const formatData = `
+Country: ${data[0][0].country}
+Cases: ${data[0][0].cases}
+Deaths: ${data[0][0].deaths}
+Recovered: ${data[0][0].recovered}
+    `;
+        ctx.reply(formatData);
+    } catch {
+        console.log('Error');
+        ctx.reply('Error. Wrong country!');
+    }
+});
+
+bot.hears('deaths' || 'Deaths', async(ctx) => {
+    let dataChangeDeaths = {};
+    try {
+        dataChangeDeaths = await api.getDeaths(ctx.message.text);
+        const formatDataChangeDeaths = `
+Date: ${dataChangeDeaths[0][0].Date}
+TotalDeaths: ${dataChangeDeaths[0][0].totalDeaths}
+ChangeInTotal: ${dataChangeDeaths[0][0].changeInTotal}
+ChangeTotalInPercent: ${dataChangeDeaths[0][0].ChangeTotalInPercent}
+    `;
+        ctx.reply(formatDataChangeDeaths);
+    } catch {
+        console.log('Error');
+        ctx.reply('Error. Try "deaths" or "Deaths"!');
+    }
+});
+
+
 // eslint-disable-next-line prettier/prettier
 bot.launch();
 
